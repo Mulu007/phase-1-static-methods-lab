e38080251e80fa9dc796bad4a9fe71af702d50b5
Mulu <brianmulu007@gmail.com>
2022-09-04 21:57:43 +0300

    completed the assignment apart from instruction 3

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  // charAt() returns the character at a specified index (position) in a string.
+  static capitalize(string) {
+    let firstLetter = string.charAt(0).toUpperCase;
+    return string.replace(string.charAt(0), firstLetter)
+  }
+  // this method takes in a string and removes all non-alphanumeric characters except for dashes, single quotes and spaces
+  static sanitize(string) {
+    return string.replace(/[^-,'A-Za-z0-9]+/g, '');
+  }
+  // static titleize(string) {
+  //   if (string = "the" || "a" || "an" || "but" || "of" || "and" || "for" || "at" || "by" || "from") {
+  //     return string.toLowerCase();
+  //   } else {
+  //     return string.toUpperCase();
+  //   }
+  // }
 }
